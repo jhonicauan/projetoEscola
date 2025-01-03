22e0764d7a93962723c880dbeaa7d8c84ee23f67
jhonielias <jhonielias8@gmail.com>
2025-01-03 02:36:48 -0300

    projeto iniciado

---
+++ sistemaEscola/src/main/java/jhonilavan/sistemaEscola/SistemaEscolaApplication.java
@@ -0,0 +1,13 @@
+package jhonilavan.sistemaEscola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEscolaApplication.class, args);
+	}
+
+}
---
+++ sistemaEscola/src/test/java/jhonilavan/sistemaEscola/SistemaEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package jhonilavan.sistemaEscola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
